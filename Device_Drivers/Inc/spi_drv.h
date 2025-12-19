da6a8412dfca55d64075f9c530b9d080ccefa5f9
ajozi <anishjoshi1986@gmail.com>
2025-12-18 21:01:41 -0500

    no message

---
+++ Device_Drivers/Inc/spi_drv.h
@@ -0,0 +1,13 @@
+/*
+ * spi_drv.h
+ *
+ *  Created on: Dec 18, 2025
+ *      Author: anish
+ */
+
+#ifndef INC_SPI_DRV_H_
+#define INC_SPI_DRV_H_
+
+
+
+#endif /* INC_SPI_DRV_H_ */
---
+++ Device_Drivers/Src/spi_drv.c
@@ -0,0 +1,10 @@
+/*
+ * spi_drv.c
+ *
+ *  Created on: Dec 18, 2025
+ *      Author: anish
+ */
+
+#include "../../HAL/Inc/spi.h"
+
+uint16_t x = 0;
