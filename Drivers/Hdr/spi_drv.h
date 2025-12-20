d7288d2bcd61958fc35adb7abf5c0cd23f139ef8
ajozi <anishjoshi1986@gmail.com>
2025-12-20 01:13:04 -0500

    no message

--- Drivers/Hdr/spi_drv.h
+++ Drivers/Hdr/spi_drv.h
@@ -8,6 +8,6 @@
 #ifndef SPI_DRV_H_
 #define SPI_DRV_H_
 
-
+uint16_t dummy(void);
 
 #endif /* HDR_SPI_DRV_H_ */
--- Drivers/Src/spi_drv.c
+++ Drivers/Src/spi_drv.c
@@ -7,6 +7,22 @@
 
 #include "../../HAL/Hdr/spi_hal.h"
 
-uint16_t x;
-SPI_Handle_st SPI_Gyro;
-//x = SPI_Read(&SPI_Gyro);
+uint16_t dummy()
+{
+	SPI_Handle_st SPI_Gyro;
+
+	/*
+	SPI_Gyro.pSPIX = pSPI1;
+	SPI_Gyro.SPI_Cfg.master = TRUE;
+	SPI_Gyro.SPI_Cfg.bustype = SPI_BUSTYPE_HDX;
+	SPI_Gyro.SPI_Cfg.rxonly = FALSE;
+	SPI_Gyro.SPI_Cfg.speed = 7;
+	SPI_Gyro.SPI_Cfg.dff = FALSE;
+	SPI_Gyro.SPI_Cfg.cpha = TRUE;
+	SPI_Gyro.SPI_Cfg.cpol = TRUE;
+	SPI_Gyro.SPI_Cfg.ssm = FALSE;
+	SPI_Gyro.SPI_Cfg.crcen = FALSE;
+*/
+	return (SPI_Read(&SPI_Gyro));
+
+}
